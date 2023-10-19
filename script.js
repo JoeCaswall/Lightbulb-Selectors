c51b6e1f166265ff21c7ba1cdae6ef1905a902a9
JoeCaswall <joe.caswall@gmail.com>
2023-10-19 11:35:54 +0100

    init comit

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Write your code here
+const lightbulb1 = document.querySelector("#lightbulb1");
+const lightbulb2 = document.querySelector("#lightbulb2");
+const lightbulb3 = document.querySelector("#lightbulb3");
+const subtitle = document.querySelector(".subtitle");
+let count = 0;
+
+function changeState() {
+  count++;
+  if (count === 1) {
+    subtitle.innerHTML = `You've clicked the light ${count} time`;
+  } else {
+    subtitle.innerHTML = `You've clicked the light ${count} times`;
+    this.classList.toggle("active");
+  }
+}
+
+lightbulb1.addEventListener("click", changeState);
+lightbulb2.addEventListener("click", changeState);
+lightbulb3.addEventListener("click", changeState);
