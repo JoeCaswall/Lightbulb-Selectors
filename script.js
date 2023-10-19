50a027dc29413a868590d88c50cbec9f8e8d4f54
JoeCaswall <joe.caswall@gmail.com>
2023-10-19 14:41:59 +0100

    added querySelectorAll

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const lightbulb1 = document.querySelector("#lightbulb1");
-const lightbulb2 = document.querySelector("#lightbulb2");
-const lightbulb3 = document.querySelector("#lightbulb3");
+const lightbulbs = document.querySelectorAll(".lightbulb");
 const subtitle = document.querySelector(".subtitle");
 let count = 0;
 
 function changeState() {
   count++;
+  this.classList.toggle("active");
   if (count === 1) {
     subtitle.innerHTML = `You've clicked the light ${count} time`;
   } else {
     subtitle.innerHTML = `You've clicked the light ${count} times`;
-    this.classList.toggle("active");
   }
 }
 
-lightbulb1.addEventListener("click", changeState);
-lightbulb2.addEventListener("click", changeState);
-lightbulb3.addEventListener("click", changeState);
+for (x of lightbulbs) {
+  x.addEventListener("click", changeState);
+}
